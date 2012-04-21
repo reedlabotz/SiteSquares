ee730a947d23242961d80ee72f013fbb805e3015
Reed La Botz <reedlabotz@gmail.com>
2012-04-20 20:37:53 -0500

    it works i think

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ import json
 
 
 app = Flask(__name__)
+app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
@@ -17,27 +18,35 @@ class Color(db.Model):
     def __init__(self,color):
         self.color = color
         self.timestamp = datetime.now()
-
     def __repr__(self):
-        return "<Color %s,%s>" % (self.datetime,self.color)
+        return "<Color %s>" % self.color
+    
+    @property
+    def serialize(self):
+        return {'id':self.id,'color':self.color,'timestamp':self.timestamp.strftime('%Y-%m-%dT%H:%M:%S')}
 
 @app.route('/')
 def hello():
     colors = Color.query.all()
-    print colors
-    return "hi"
+    return json.dumps([i.serialize for i in colors])
 
 @app.route('/color/<color>')
 def color(color):
     try:
+        if not (int(color,16) < 16777216):
+            return "bad color"
+    except:
+        return "bad color"
+    try:
         c = Color(color)
         db.session.add(c)
         db.session.commit()
-    except Exception as inst:
-        print inst
+    except:
+        return "error"
     return "thanks"
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    db.create_all()
     app.run(host='0.0.0.0', port=port)
