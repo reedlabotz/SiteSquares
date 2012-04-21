bbb9cea866e1b5794d4b7969c09c7cfcbb511dbd
Reed La Botz <reedlabotz@gmail.com>
2012-04-20 20:00:23 -0500

    save working maybe

--- server/app.py
+++ server/app.py
@@ -1,50 +1,34 @@
 import os
-import sys
-import urlparse
-
-# Register database schemes in URLs.
-urlparse.uses_netloc.append('postgres')
-urlparse.uses_netloc.append('mysql')
-
-try:
-
-    # Check to make sure DATABASES is set in settings.py file.
-    # If not default to {}
-
-    if 'DATABASES' not in locals():
-        DATABASES = {}
-
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
-
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        })
-        if url.scheme == 'postgres':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except Exception:
-    print 'Unexpected error:', sys.exc_info()
-
-
-import os
-
+from datetime import datetime
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class Color(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    color = db.Column(db.String(6))
+    timestamp = db.Column(db.DateTime())
+
+    def __init__(self,color):
+        self.color = color
+        self.timestamp = datetime.now()
+
+    def __repr__(self):
+        return "<Color %s,%s>" % (self.datetime,self.color)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/color/<color>')
+def color(color):
+    c = Color(color)
+    db.session.add(c)
+    db.session.commit()
+    return "thanks: %s"%c
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
