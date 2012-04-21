3f21cca60a55c9728ef2d721774ce1f0ced1e575
RJ <rj@rj-MacBook.(none)>
2012-04-21 00:22:43 -0500

    Actually working in the background

--- chrome-extension/colors.js
+++ chrome-extension/colors.js
@@ -1,8 +1,13 @@
 
-function tryIcon(faviconUrl) {
+var tabcolors = {};
+
+
+
+function tryIcon(faviconUrl, url, shouldsend) {
 	function onImageLoaded() {
-		console.log("image loaded");
-		findColorOfIcon(img);
+		//console.log("image loaded");
+		colors = findColorOfIcon(img);
+		setColor(url, colors, shouldsend);
 	};
 	var img = document.createElement("img");
 	img.addEventListener("load", onImageLoaded);
@@ -38,19 +43,24 @@ function findColorOfIcon(image) {
 			}
 		}
 	}
-	console.log("color of icon: "+colors);
-	//send the data to the extension
-	chrome.extension.sendRequest({url:document.location.href, colors: colors}, function(response) {
-	});
+	//console.log("color of icon: "+colors);
+	return colors;
+}
+function isValidColor(r,g,b,a) {
+	if (a < 20) return false;
+	if (r < 20 && g < 20 && b < 20) return false;
+	if (r > 240 && g > 240 && b > 240) return false;
+
+	return true;
 }
 
 
 
-var tabcolors = {};
 
 
 function findColorOfTab() {
 	runOnCurrentTab(function(tab1) {
+		tryIcon(tab1.favIconUrl, tab1.url, false);
 		window.setTimeout(function() {
 			runOnCurrentTab(function(tab2) {
 				if (tab1.id == tab2.id) {
@@ -60,8 +70,8 @@ function findColorOfTab() {
 					var color = tabcolors[this.url];
 					if (color) 
 						sendColor(color);
-					else
-						tryIcon(tab2.favIconUrl);
+					else 
+						tryIcon(tab2.favIconUrl, tab2.url, true);
 				} else {
 					console.log("Not teh same tab!");
 				}
@@ -88,14 +98,14 @@ function getMaxColor(colors) {
 	var argmax;
 	for (key in colors) {
 		var element = colors[key];
-		console.log("Color: "+key+" times: "+element);
+		//console.log("Color: "+key+" times: "+element);
 		if (element > max) {
 			max = element;
 			argmax = key;
 		}
 	}
 
-	console.log(" Best color:" +argmax + " with "+max+" many");
+	//console.log(" Best color:" +argmax + " with "+max+" many");
 	return argmax;
 }
 
@@ -103,33 +113,31 @@ function getMaxColor(colors) {
 function sendColor(color) {
 	var url = "http://sitesquares.herokuapp.com/color/"+color;
 	console.log("Sending to url: "+url);
-	//$.get(url, function() {
-	//	console.log("Success");
-	//});
-}
-
-
-function askTabColor() {
-	chrome.tabs.getSelected(null, function(tab) {
-		chrome.tabs.sendRequest(tab.id, {action: "gimmecolor"}, function(response) {
-		});
+	$.get(url, function() {
+		console.log("Success");
 	});
 }
 
+function setColor(url, colors, send) {
+	//console.log("Lots of colors! "+url);
+	var color = getMaxColor(colors);
+	tabcolors[url] = color;
+	//console.log("color here: "+color);
+	if (send)
+		sendColor(color);
+}
 //recieve the colors from the pages
 chrome.extension.onRequest.addListener( function(request, sender, sendResponse) {
-	console.log("Lots of colors! "+request.url);
-	tabcolors[request.url] = getMaxColor(request.colors);
-	console.log(tabcolors);
+	tryIcon(request.favicon, request.url, true);
 });
 
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	console.log("Tab: "+tab+" tabId: "+tabId);
+	//console.log("Tab: "+tab+" tabId: "+tabId);
 	findColorOfTab(tab);
 });
 chrome.tabs.onCreated.addListener(function(tab) {
-	console.log("Tab: "+tab);
+	//console.log("Tab: "+tab);
 	findColorOfTab(tab);
 });
 //chrome.tabs.OnRemoved.addListener(function(tab) {
@@ -137,7 +145,7 @@ chrome.tabs.onCreated.addListener(function(tab) {
 //	findColorOfTab(tab);
 //});
 chrome.tabs.onActiveChanged.addListener(function(activeInfo) {
-	console.log("ACTIVATED: "+activeInfo);	
+	//console.log("ACTIVATED: "+activeInfo);	
 	findColorOfTab();	
 });
 //chrome.tabs.onActivated.addListener(function(activeInfo) {
